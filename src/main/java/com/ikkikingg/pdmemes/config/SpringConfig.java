786fa2281f8aef5a04403808e428d8e03f724f71
IkkiKing <40593337+IkkiKing@users.noreply.github.com>
2021-11-16 16:05:53 +0300

    Initial commit

--- src/main/java/com/ikkikingg/pdmemes/config/SpringConfig.java
+++ src/main/java/com/ikkikingg/pdmemes/config/SpringConfig.java
@@ -10,14 +10,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 @Configuration
 @Data
@@ -40,28 +38,30 @@ public class SpringConfig {
     @Bean
     public Page getActualPage() {
         Page page = new Page();
-        try {
-            page = getObjectMapper().readValue(readFileFromResources("/page.json"), Page.class);
-        } catch (Exception e) {
-            log.error("Error while reading json page file: ");
+        try (InputStream inputStream = getClass().getResourceAsStream("/page.json");
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            String contents = reader.lines()
+                    .collect(Collectors.joining(System.lineSeparator()));
+            page = getObjectMapper().readValue(contents, Page.class);
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return page;
     }
 
-    public String readFileFromResources(String filename) throws Exception {
-        Class clazz = SpringConfig.class;
-        InputStream inputStream = clazz.getResourceAsStream(filename);
-        return readFromInputStream(inputStream);
-    }
+//    public String readFileFromResources(String filename) throws Exception {
+//        Class clazz = SpringConfig.class;
+//        InputStream inputStream = clazz.getResourceAsStream(filename);
+//        return readFromInputStream(inputStream);
+//    }
 
-    private String readFromInputStream(InputStream inputStream) throws Exception {
-        ByteArrayOutputStream result = new ByteArrayOutputStream();
-        byte[] buffer = new byte[1024];
-        for (int length; (length = inputStream.read(buffer)) != -1; ) {
-            result.write(buffer, 0, length);
-        }
-        // StandardCharsets.UTF_8.name() > JDK 7
-        return result.toString("UTF-8");
-    }
+//    private String readFromInputStream(InputStream inputStream) throws Exception {
+//        ByteArrayOutputStream result = new ByteArrayOutputStream();
+//        byte[] buffer = new byte[1024];
+//        for (int length; (length = inputStream.read(buffer)) != -1; ) {
+//            result.write(buffer, 0, length);
+//        }
+//        // StandardCharsets.UTF_8.name() > JDK 7
+//        return result.toString("UTF-8");
+//    }
 }
